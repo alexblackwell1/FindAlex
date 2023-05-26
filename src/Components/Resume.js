16219ebf74187495968ffb155cf74a4e1e954fff
Alex <blackwellag@appstate.edu>
2023-05-26 12:26:35 -0400

    resume displayed, extra space

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import { Document, Page, pdfjs } from "react-pdf";
+import resume from '../styling/assets/resume.pdf';
 import '../styling/default.css';
 import '../styling/resume.css';
-import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
-  const [error, setError] = useState(null);
+    const handleScroll = (e) => {
+        const container = e.target;
+        if (container.scrollTop + container.offsetHeight >= container.scrollHeight) {
+          container.scrollTop = container.scrollHeight - container.offsetHeight;
+        }
+      };
 
-  const handleDocumentLoadError = (error) => {
-    setError(error);
-    console.error('Error: loading PDF:', error);
-  };
-
-  return (
-    <div className='resume-page'>
-      <div className='resume-title'>
-        <Link to='/' className='return'><h1>◄</h1></Link>
-        <h1>Résumé</h1>
+    return (
+      <div className="resume-page">
+            <div className="resume-title">
+                <Link to="/" className="return">
+                    <h1>◄</h1>
+                </Link>
+                <h1>Résumé</h1>
+            </div>
+            <div className="resume-body">
+                <div className="pdf-container" onScroll={handleScroll}>
+                    <Document file={resume}>
+                        <Page pageNumber={1} renderTextLayer={false} width={800} />
+                    </Document>
+                </div>
+            </div>
       </div>
-      <div className='resume-body'>
-        {error ? (
-          <div className='error-message'>Error loading PDF</div>
-        ) : (
-          <Document file={'./resume.pdf'} onLoadError={handleDocumentLoadError}>
-            <Page pageNumber={1} />
-          </Document>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Resume;
